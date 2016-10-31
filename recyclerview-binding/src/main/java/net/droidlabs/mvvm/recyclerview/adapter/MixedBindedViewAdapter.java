330fc0b426f166bdc4f8843dfb85665e0ae884b3
Quentin Gaultier <quentin.gaultier@gmail.com>
2016-10-31 10:21:28 +0800

    support for mixed content (stub)

---
+++ recyclerview-binding/src/main/java/net/droidlabs/mvvm/recyclerview/adapter/MixedBindedViewAdapter.java
@@ -0,0 +1,79 @@
+package net.droidlabs.mvvm.recyclerview.adapter;
+
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import net.droidlabs.mvvm.recyclerview.adapter.binder.ItemBinder;
+
+import java.util.Collection;
+
+/**
+ * Created by q on 30/10/16.
+ */
+
+public class MixedBindedViewAdapter<T>  extends BindingRecyclerViewAdapter<T> {
+
+
+    private View.OnClickListener mixedHandler;
+    private boolean showMixed = true;
+    private int mixedLayout;
+
+
+    public MixedBindedViewAdapter(ItemBinder<T> binder, @Nullable Collection<T> items, int mixedLayout) {
+        super(binder, items);
+        this.mixedLayout = mixedLayout;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return isMixedItem(position) ?
+                this.mixedLayout :
+                super.getItemViewType(position);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        if (isMixedItem(position)) {
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mixedHandler.onClick(v);
+                }
+            });
+        } else {
+            super.onBindViewHolder(viewHolder, position);
+        }
+    }
+
+    @Override
+    protected void bindView(View root, T item, int position) {
+        if (isMixedItem(position)) {
+            assignItem(root, null);
+            root.setOnClickListener(mixedHandler);
+            root.setOnLongClickListener(null);
+        } else {
+            super.bindView(root, item, position);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return super.getItemCount() + (this.showMixed ? 1 : 0);
+    }
+
+    boolean isMixedItem(int position) {
+        return this.showMixed && position == getItemCount() - 1;
+    }
+
+    public void setMixedHandler(View.OnClickListener mixedHandler) {
+        this.mixedHandler = mixedHandler;
+    }
+
+    public void setShowMixed(boolean show) {
+        if (this.showMixed != show) {
+            this.showMixed = show;
+            notifyDataSetChanged();
+        }
+    }
+}
